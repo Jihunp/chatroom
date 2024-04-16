a4ec13fb5b78616c985b94c298fb046ea184652d
jihunp <edwardpark.park@gmail.com>
2024-04-15 23:58:26 -0700

    add text to box

--- app/javascript/controllers/chatroom_controller.js
+++ app/javascript/controllers/chatroom_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["input", "chatbox"]
+
   connect() {
     console.log("connected let's kick some ass")
+  }
+
+  send() {
+    this.chatboxTarget.innerHTML += `<div>${this.inputTarget.value}</div>`
+    this.inputTarget.value
   }
 }
